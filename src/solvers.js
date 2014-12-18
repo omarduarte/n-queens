b75a036c7ca25eff893a8da1beba62027c4f6521
omarduarte <omarduarte@gmail.com>
2014-12-17 20:52:29 -0800

    Better commenting and code clean up

--- src/solvers.js
+++ src/solvers.js
@@ -18,7 +18,7 @@ window.findNRooksSolution = function (n) {
 
   for (var row = 0; row < n; row++) {
     for (var col = 0; col < n; col++) {
-      var currentRow = board.get(row);
+      var currentRow = board.get(row); //select the current row during the iteration
       currentRow[col] = 1; //set column j in current row to 1
       board.set(row, currentRow); //push this to the board
       if (board.hasAnyRooksConflicts()) { //if it fails
@@ -32,10 +32,9 @@ window.findNRooksSolution = function (n) {
   for (var row  = 0; row < n; row++) {
     matrix.push(board.get(row));
   }
-  var solution = matrix; //fixme
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(matrix));
+  return matrix;
 };
 
 
@@ -83,24 +82,21 @@ window.findNQueensSolution = function (n) {
     if (rowIndex === n) {
       return makeSolution(); //if we find a solution
     }
-    //create a new row from rowIndex taken from the board instance
-    var currentRow = board.get(rowIndex);
+
+    var currentRow = board.get(rowIndex); //create a new row from rowIndex taken from the board instance
     //iterate through each row
     for (var col = 0; col < currentRow.length; col++) {
-      //insert a 1 at column and check for conflicts, increase counter
-      currentRow[col] = 1;
-      board.set(rowIndex, currentRow);
-      //if any conflicts,
-      if (board.hasAnyQueensConflicts()) {
-        //restore column and go to next column
-        currentRow[col] = 0;
-        board.set(rowIndex, currentRow);
-        continue;
+      currentRow[col] = 1; //insert a 1 at column and check for conflicts, increase counter
+      board.set(rowIndex, currentRow); //update the board
+
+      if (board.hasAnyQueensConflicts()) { //if any conflicts,
+        currentRow[col] = 0; //restore column
+        board.set(rowIndex, currentRow); //reset the board
+        continue; //go to next column
       } else {
-        //recurse into to next row. Pass next rowIndex
-        var result = recursion(rowIndex + 1);
-        // if result's recursion passes the base condition
-        if (result) {
+        var result = recursion(rowIndex + 1); //recurse into to next row. Pass next rowIndex
+
+        if (result) { // if result's recursion passes the base condition
           return result; //return the solution
         } else {
           currentRow[col] = 0; //reset the currentCol's value to 0
@@ -108,8 +104,8 @@ window.findNQueensSolution = function (n) {
         }
       }
     }
-    // Only if result === null;
-    return result;
+
+    return result; // Only if result === null;
   };
 
   return recursion(0); //call it
